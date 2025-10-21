1e8a98d73e8779f89c4abd47c0727addb53e0c88
김수현(hvl1789) <hvl1789@gepplus.net>
2025-10-21 14:05:47 +0900

    API

--- src/hooks/useNewsApi.js
+++ src/hooks/useNewsApi.js
@@ -1,31 +1,16 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
-export const useNewsApi = (category = "General") => {
+export const useNewsApi = (category = "general") => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
-  const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-//   console.log("category1:", category);
-  const query = category || 'general';
-
-  if(import.meta.env.VITE_NEWS_API_KEY){
-      console.log("돌아와제발");
-  }
-  console.log("category2:", category, query);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
         setLoading(true);
-        const res = await axios.get("https://gnews.io/api/v4/search", {
-          params: {
-            q: query,
-            lang: "en",
-            max: 5,
-            token: API_KEY,
-          },
-        });
-        setArticles(res.data.articles);
+        const res = await fetch(`/api/news?category=${category}`);
+        const data = await res.json();
+        setArticles(data);
       } catch (err) {
         setArticles([]);
       } finally {
